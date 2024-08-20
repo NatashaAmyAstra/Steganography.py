9c2ee2c40cc9485b993871e9dde8f013da3a5810
Natasha Amy Astra <NatashaAmyAstra@gmail.com>
2024-08-20 23:15:27 +0200

    functional build 1

--- imageReadingTest.py
+++ imageReadingTest.py
@@ -11,27 +11,39 @@ import random
 def encodeBinary(message):
     return ''.join(format(ord(i), '08b') for i in message)
 
-# translate binary to text
-def decodeBinary(message):    
+def decodeBinary(binary):
     result = ''
-    for i in range(int(len(message) / 8)):
-        result += chr(int(message[i * 8 : i * 8 + 8], 2))
+
+    # run this next section once for every 8 bit section in given binary string
+    for i in range(int(len(binary) / 8)):
+        # translate each 8 bit section into an integer value and in turn into a character
+        result += chr(int(binary[i * 8 : i * 8 + 8], 2))
 
     return result
 
 
 def encodeColor(color, binary):
+    # translate the binary into its integer equivalent
     value = int(binary, 2)
+
+    # change color to the nearest multiple of 16 below itself
+    # this essentially creates a blank canvas
     color -= color % 16
+
+    # add the value onto the color. color % 16 = value
     color += value
 
+    # make sure the color is within a valid range
     if color > 255:
         color -= 16
 
     return color
 
+
 def decodeColor(color):
+    # extract encoded value and convert to a 4 bit string
     return format(color % 16, '04b')
+
 
 def arrayToThreeIndices(arr, x):
     # a, b and c represent the length of each depth of the array
@@ -44,14 +56,24 @@ def arrayToThreeIndices(arr, x):
     j = math.floor((x/c) % b)
     k = x % c
 
+    # return indices as a tuple
     return i, j, k
 
-def lengthAllocation(img){
+def lengthAllocation(img):
+    # writing 4 bits into each R, G and B allows for 12 bits per pixel
+    # a character is 8 bits, thus 2 pixels fit exactly 3 characters
+    # the maximum number of characters in an image is thus 1.5 * resolution
     maxChrs = 1.5 * len(img) * len(img[0])
+
+    # each image will have a set number of bits allocated to say how long
+    # the message encoded in the image is
+    # the size of this allocation is the lowest x where 2 ^4x - 1 > 1.5*resolution
     lenAll = 1
     while pow(2, 4 * lenAll) - 1 < maxChrs:
         lenAll += 1
 
+    # due to this allocation, less bits are actually available for text thus
+    # maxChrs is slightly decreased
     maxChrs -= math.ceil(lenAll/2)
 
     return lenAll, maxChrs
@@ -59,26 +81,48 @@ def lengthAllocation(img){
 
 # encode message into image
 def encodeImage(message, imagePath):
-    binary = encodeBinary(message)
-
     img = iio.imread(imagePath)
+    lenAll, maxChr = lengthAllocation(img)
 
+    if len(message) > maxChr:
+        print("message too long for this image size,\nplease choose smaller image or shorten your message")
+        return
+
+    # before the message, some data is encoded to tell the computer how long the message is
+    # space for this is allocated relative to image resolution to ensure we can always decode it
+    lengthText = format(len(message), '0'+str(lenAll * 4)+'b')
+    binary = lengthText + encodeBinary(message)
+
+    # encode data into image 4 bits at a time
     for x in range(int(len(binary) / 4)):
+        # convert the linear x to 3 indices for each unique position
         i, j, k = arrayToThreeIndices(img, x)
 
-        # which section of binary is going to be encoded next
+        # encode the 4 bits into the color value of the current pixel
         section = binary[x*4 : x*4 +4]
         img[i][j][k] = encodeColor(img[i][j][k], section)
 
     iio.imwrite(imagePath, img)
 
-# extract text from image
-def decodeImage(imagePath, length):
-    img = iio.imread(imagePath)
-    result = ''
 
-    for x in range(length):
+def readColors(img, start, end):
+    binary = ''
+
+    for x in range(start, end):
         i, j, k = arrayToThreeIndices(img, x)
-        result += decodeColor(img[i][j][k])
+        binary += decodeColor(img[i][j][k])
 
-    return decodeBinary(result)
+    return binary
+
+# extract text from image
+def decodeImage(imagePath):
+    img = iio.imread(imagePath)
+    
+    # read how long the message is
+    lenAll = lengthAllocation(img)[0]
+    messageLen = int(readColors(img, 0, lenAll), 2) * 2
+    
+    return decodeBinary(readColors(img, lenAll, lenAll + messageLen))
+
+
+
